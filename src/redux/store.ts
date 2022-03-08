111fbbdc4da207b544dae57edf33f67cb1c5b93b
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-03-08 19:45:38 +0300

    Added localStorage.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,18 @@
 import {combineReducers, createStore} from "redux";
 import {counterReducer} from "./counterReducer";
+import {loadState, saveState} from "./localStorage";
 
 const rootReducer = combineReducers({
     counter: counterReducer,
 })
+const persistedState = loadState();
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, persistedState)
+
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+    });
+});
 
 export type AppStateType = ReturnType<typeof rootReducer>
